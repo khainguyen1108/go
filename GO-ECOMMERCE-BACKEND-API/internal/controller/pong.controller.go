9d78225766f3a1d4ef0a7a045a3e8a55002a846f
khainguyen1108 <khai1108070707@gmail.com>
2026-03-13 17:30:57 +0700

    abc

--- GO-ECOMMERCE-BACKEND-API/internal/controller/pong.controller.go
+++ GO-ECOMMERCE-BACKEND-API/internal/controller/pong.controller.go
@@ -6,9 +6,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/openai/openai-go/v3"
-	"github.com/openai/openai-go/v3/option"
-	"github.com/openai/openai-go/v3/responses"
 	"google.golang.org/genai"
 )
 
@@ -35,16 +32,5 @@ func (tc TestController) Pong(c *gin.Context) {
 }
 
 func (tc TestController) PongOpenApi(c *gin.Context) {
-	client := openai.NewClient(
-		option.WithAPIKey("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"),
-	)
 
-	resp, err := client.Responses.New(context.TODO(), responses.ResponseNewParams{
-		Model: "gpt-5.4",
-		Input: responses.ResponseNewParamsInputUnion{OfString: openai.String("Say this is a test")},
-	})
-	if err != nil {
-		panic(err.Error())
-	}
-	c.JSON(http.StatusOK, gin.H{"response": resp.OutputText()})
 }
